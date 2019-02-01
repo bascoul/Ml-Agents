7472dcc4dfdab7ce0dadd5782b97f952fb6e2f1f
Jonathan Harper <jharper@unity3d.com>
2019-02-01 15:53:50 -0800

    Move 'take_action' into Policy class
    This refactor is part of Actor-Trainer separation. Since policies
    will be distributed across actors in separate processes which share
    a single trainer, taking an action should be the responsibility of
    the policy.
    
    This change makes a few smaller changes:
    * Combines `take_action` logic between trainers, making it more
      generic
    * Adds an `ActionInfo` data class to be more explicit about the
      data returned by the policy, only used by TrainerController and
      policy for now.
    * Moves trainer stats logic out of `take_action` and into
      `add_experiences`
    * Fixes a minor issue where `reward_buffer` was being referred to
      on generic Trainers during curriculum learning even though this
      property doesn't exist on the base Trainer class.

--- ml-agents/mlagents/trainers/ppo/policy.py
+++ ml-agents/mlagents/trainers/ppo/policy.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from mlagents.trainers.ppo.models import PPOModel
 from mlagents.trainers.policy import Policy
+from mlagents.envs import BrainInfo
 
 logger = logging.getLogger("mlagents.trainers")
 
@@ -60,7 +61,7 @@ class PPOPolicy(Policy):
             self.update_dict['forward_loss'] = self.model.forward_loss
             self.update_dict['inverse_loss'] = self.model.inverse_loss
 
-    def evaluate(self, brain_info):
+    def evaluate(self, brain_info: BrainInfo):
         """
         Evaluates policy for the agent experiences provided.
         :param brain_info: BrainInfo object containing inputs.
